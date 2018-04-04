370621fdc7da82aecec1763621cfadad7181b1c7
vandana <vandanayadav842@gmail.com>
2018-04-04 18:45:37 +0530

    update

--- modals/admin.js
+++ modals/admin.js
@@ -21,6 +21,7 @@ exports.selectQuery = function(table_name,value,callback) {
         connection.query(sql, el, function(err, result){
             err ? reject(err) : resolve(result);
         });
+   
     });
 };
 exports.insertQuery = function(table_name,value,callback) {
